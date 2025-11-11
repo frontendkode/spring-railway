cc9f521160c129c2a3b58faff651aef4e1f45c60
frontendkode <frontendkodedev@gmail.com>
2025-11-11 18:15:26 +0530

    koko

--- src/main/java/com/KissTech/crm/config/WebSecurityConfig.java
+++ src/main/java/com/KissTech/crm/config/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200/","https://kisstech.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
